72b479b0d6a2368fc16f2da64d8345b299875fb7
eugeniomorocho <sf4e@outlook.com>
2025-09-17 21:06:40 -0500

    Added app_square.py

--- 8. API Frameworks/Streamlit/app_square.py
+++ 8. API Frameworks/Streamlit/app_square.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title(""💻 Gráfico de una función cuadrática"")
+st.title("💻 Gráfico de una función cuadrática")
 
 # Slider
 val = st.slider("Número", -10, 10, 0)
